419f21f8a42722fab90177e2139b8566ccb6dcc4
Eliazid Boulaamail <eliazidb@gmail.com>
2023-10-12 10:50:24 +0200

    création du squelette

---
+++ GSB-FINAL/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSB_FINAL
+{
+    static class Modele
+    {
+        private static ALaSmeneProEntities maConnexion;
+        private static 
+    }
+}
