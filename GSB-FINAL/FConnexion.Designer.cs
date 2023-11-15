3e3d08aeb84e4d57e87e3c8dfdb6930045325d6c
Eliazid Boulaamail <eliazidb@gmail.com>
2023-11-15 11:17:46 +0100

    Page de connexion effectuer

--- GSB-FINAL/FConnexion.Designer.cs
+++ GSB-FINAL/FConnexion.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.identifiant = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mdp = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.infoconnection = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(388, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "identifiant :";
+            // 
+            // identifiant
+            // 
+            this.identifiant.Location = new System.Drawing.Point(365, 160);
+            this.identifiant.Name = "identifiant";
+            this.identifiant.Size = new System.Drawing.Size(115, 20);
+            this.identifiant.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(377, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mot de passe : ";
+            // 
+            // mdp
+            // 
+            this.mdp.Location = new System.Drawing.Point(365, 267);
+            this.mdp.Name = "mdp";
+            this.mdp.Size = new System.Drawing.Size(115, 20);
+            this.mdp.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(380, 315);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Valider";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // infoconnection
+            // 
+            this.infoconnection.AutoSize = true;
+            this.infoconnection.Location = new System.Drawing.Point(391, 396);
+            this.infoconnection.Name = "infoconnection";
+            this.infoconnection.Size = new System.Drawing.Size(0, 13);
+            this.infoconnection.TabIndex = 5;
+            // 
+            // FConnexion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(890, 508);
+            this.Controls.Add(this.infoconnection);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.mdp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.identifiant);
+            this.Controls.Add(this.label1);
+            this.Name = "FConnexion";
             this.Text = "FConnexion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox identifiant;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox mdp;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label infoconnection;
     }
 }
---
+++ GSB-FINAL/ModeleConnexion.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSB_FINAL
+{
+    internal class ModeleConnexion
+    {
+        public static ALaSmeneProEntities maConnexion;
+        private static Visiteur leVisiteurConnecte;
+        public static bool connexValid;
+
+        public static Visiteur LeVisiteurConnecte { get => leVisiteurConnecte; set => leVisiteurConnecte = value; }
+        public bool ConnexValid { get => connexValid; set => connexValid = value; }
+
+        public static void init()
+        {
+            maConnexion = new ALaSmeneProEntities();
+        }
+        private static string GetMd5Hash(string PasswdSaisi)
+        {
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(PasswdSaisi);
+            byte[] hash = (MD5.Create()).ComputeHash(inputBytes);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+        public static string connex(string id, string mdp)
+        {
+            string msg = "";
+            List<Visiteur> user = maConnexion.Visiteur.Where(x => x.identifiant == id).ToList();
+            if(user.Count == 0)
+            {
+                msg = "Utilisateur innexistant";
+                connexValid = false;
+            }
+            else
+            {
+                if (user.Count > 1)
+                {
+                    msg = "erreur dans la base de données, contactez l'administrateur de la base de données";
+                    connexValid = false;
+                }
+                else
+                {
+                    leVisiteurConnecte = user[0];
+                    string mdph = GetMd5Hash(mdp);
+                    string md = LeVisiteurConnecte.password.Substring(2).ToString();
+                    if (String.Equals(mdph, LeVisiteurConnecte.password.Substring(2).ToString(),StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        msg = "Bienvenue"+" "+leVisiteurConnecte.prenom;
+                        connexValid = true;
+                    }
+                    else
+                    {
+                        msg = "identifiant ou Mot de passe incorect ";
+                        connexValid = false;
+                    }
+
+                }
+            }
+            return msg;
+        }
+       
+    }
+}
